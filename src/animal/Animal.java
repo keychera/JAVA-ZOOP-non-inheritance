3b48e5eb6db6723f3e5e709eedc0827be88939eb
royyandzakiy <royyan.abdul@gmail.com>
2017-03-29 11:09:39 +0700

    Animal.java added 25 animals

--- src/animal/Animal.java
+++ src/animal/Animal.java
@@ -25,6 +25,161 @@ public class Animal {
         interaction = "NYAA";
         render = 'c';
         break;
+      case "Beluga":
+        classification = new Classification("Monodontidae","Delphinapterus","leucas");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("water");
+        interaction = "SPLASH";
+        render = 'B';
+        break;
+      case "BaldEagle":
+        classification = new Classification("Accipitridae","Haliaeetus","leucocephalus");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("air");
+        interaction = "BAWK";
+        render = 'E';
+        break;
+      case "Biawak":
+        classification = new Classification("Varanidae","Varanus","cerambonensis");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "MORE TERRIFYING HSSS";
+        render = ('b');
+        break;
+      case "BlackKite":
+        classification = new Classification("Accipitridae","Milvus","migrans");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("air");
+        interaction = "dark flip flop";
+        render = ('k');
+        break;
+      case "Cheetah":
+        classification = new Classification("Felidae","Acinonyx","jubatus");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "*slightly soft* RAWR";
+        render = ('C');
+        break;
+      case "Clownfish":
+        classification = new Classification("Pomacentridae","Ocellaris","clownfish");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("water");
+        interaction = "lil blub";
+        render = ('C');
+        break;
+      case "Coyote":
+        classification = new Classification("Canidae","Canis","latrans");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        tame_status = false;
+        interaction = "AGILE RAWR";
+        render = ('t');
+        break;
+      case "Damselfish":
+        classification = new Classification("Pomacentridae","Stegastes","variabilis");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("water");
+        interaction = "lil *majestic* blub";
+        render = ('d');
+        break;
+      case "Dolphin":
+        classification = new Classification("Delphinidae","Delphinus","delphis");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("water");
+        interaction = "dolphin's SKREE";
+        render = ('D');
+        break;
+      case "GoldenEagle":
+        classification = new Classification("Accipitridae","Aquila","chrysaetos");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("air");
+        interaction = "GOLDEN BAWK";
+        render = ('e');
+        break;
+      case "Komodo":
+        classification = new Classification("Varanidae","Varanus","komodoensis");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "HSSS";
+        render = ('K');
+        break;
+      case "Leopard":
+        classification = new Classification("Felidae","Panthera","pardus");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "MAJESTIC RAWR";
+        render = ('l');
+        break;
+      case "Narwhal":
+        classification = new Classification("Monodontidae","Monodon","monoceros");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("water");
+        interaction = "NARWHAL SPLASH";
+        render = ('N');
+        break;
+      case "Orca":
+        classification = new Classification("Delphinidae","Orcinus","orca");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("water");
+        interaction = "SPLASHING RAWR";
+        render = ('O');
+        break;
+      case "RedFox":
+        classification = new Classification("Canidae","Vulpes","vulpes");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "RAWR with passion";
+        render = ('F');
+        break;
+      case "RedKite":
+        classification = new Classification("Accipitridae","Milvus","milvus");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("air");
+        interaction = "flip flop";
+        render = ('R');
+        break;
+      case "Tiger":
+        classification = new Classification("Felidae","Felis","tigris");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "COURAGEOUS RAWR";
+        render = ('T');
+        break;
+      case "Wolf":
+        classification = new Classification("Canidae","Canis","lupus");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "A STANDARD RAWR";
+        render = ('W');
+        break;
+      case "ArcticFox":
+        classification = new Classification("Canidae","Vulpes","lagopus");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "ARCTIC RAWR";
+        render = ('f');
+        break;
+      case "Axolotl":
+        classification = new Classification("Ambystomatidae","Ambystoma","mexicanum");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("water");
+        interaction = "a rare blub";
+        render = ('A');
+        break;
+      case "BadakJawa":
+        classification = new Classification("Rhinocerotidae","Rhinoceros","sondaicus");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "HMM DON'T MIND ME AND MY ENDANGERED STATUS";
+        render = ('J');
+        break;
+      case "BadakSumatra":
+        classification = new Classification("Rhinocerotidae","Dicerorhinus","sumatrensis");
+        dietStatus = new DietStatus(weight,true,false);
+        habitatStatus = new HabitatStatus("land");
+        interaction = "IM WITH JAWA";
+        render = 'S';
+        break;
       default:
         classification = new Classification("null","null","null");
         dietStatus = new DietStatus(weight,true,false);
